b637f807432d9a06e466bb6022804e2d6109ee68
chengyu_lin <chengyu_lin@aspeedtech.com>
2025-06-16 14:09:29 +0800

    caliptra: Support dependency repo clone script
    Signed-off-by: chengyu_lin <chengyu_lin@aspeedtech.com>
    Change-Id: I860b23a224a04313ea68c25fe6583f7f05313707

---
+++ scripts/dep.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+YELLOW='\033[0;33m'
+END='\033[0m'
+
+DIR="$(dirname "$(realpath "$0")")"
+CPTRA_TOOLS_DIR="$DIR/.."
+CPTRA_SW_DIR="$CPTRA_TOOLS_DIR/../caliptra-sw"
+CPTRA_MCU_SW_DIR="$CPTRA_TOOLS_DIR/../caliptra-mcu-sw"
+
+function cptra_printf() {
+    echo -e "${YELLOW}[CPTRA]${END} $1"
+}
+
+pushd .
+
+# Get caliptra-sw repository
+if [ ! -d $CPTRA_SW_DIR ]; then
+    cptra_printf "Cloning caliptra-sw repository..."
+    git clone ssh://gerrit.aspeed.com:29418/caliptra-sw $CPTRA_SW_DIR
+    cd $CPTRA_SW_DIR
+    git checkout aspeed-rt-1.2.0
+    git submodule init
+    git submodule update dpe
+else
+    cptra_printf "Caliptra-sw repository already exists."
+fi
+
+# Get caliptra-mcu-sw repository
+if [ ! -d $CPTRA_MCU_SW_DIR ]; then
+    cptra_printf "Cloning caliptra-mcu-sw repository..."
+    git clone https://github.com/chipsalliance/caliptra-mcu-sw.git $CPTRA_MCU_SW_DIR
+else
+    cptra_printf "Caliptra-mcu-sw repository already exists."
+fi
+
+popd
+
